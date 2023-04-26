4fe891f827c171a8cf2c47abe2d80e07fc8ae0b4
ZUBAIR-coder95 <118192711+zubair-devinx@users.noreply.github.com>
2023-04-26 16:18:15 +0500

    angular update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PropertiesComponent } from './properties/properties.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', redirectTo:'login', pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'signup',
+    component:SignUpComponent
+  },
+  {
+    path:'properties',
+    component:PropertiesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PropertiesComponent } from './properties/properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    PropertiesComponent
   ],
   imports: [
     BrowserModule,
